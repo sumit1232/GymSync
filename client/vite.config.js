c221fcd60168ce865df23f8c0b026850bf5a72db
sumit <sumitjadav2525@gmail.com>
2026-03-28 12:38:37 +0530

    error correction

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-    build: {
-    outDir: "dist",
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          reactVendor: ["react", "react-dom"],
+          chart: ["chart.js", "react-chartjs-2"],
+        },
+      },
+    },
   },
-})
+});
