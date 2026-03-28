cbb448e739e6e0a2ac9c0e65a5398cc0910f881c
sumit <sumitjadav2525@gmail.com>
2026-03-28 12:43:05 +0530

    vercel config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,11 @@
-// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          reactVendor: ["react", "react-dom"],
-          chart: ["chart.js", "react-chartjs-2"],
-        },
-      },
-    },
-  },
-});
+  plugins: [react(),
+    tailwindcss()
+  ],
+  
+})
